2b3f5d0fcf272593531a2b9a32196e501da58651
Aditi Patil <aditiprakashpatil7@gmail.com>
2024-09-07 00:59:02 +0530

    3rd commit center content done

--- src/Components/Page_1/Page_1.jsx
+++ src/Components/Page_1/Page_1.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import TopBar from "../TopBar"
 import './Page_1.css'
 import MidBar from '../MidBar'
+import Navbar from '../Navbar'
+
+
 const Page_1 = () => {
   return (
     <div>
         <div className="page_1">
             <TopBar/>
             <MidBar/>
+            <Navbar/>
+            {/* <Content/>*/}
 
         </div>
         
